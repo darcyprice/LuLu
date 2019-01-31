c221c6f1e61d1645b36997644e154dfea55fdcc1
darcyprice <darcyelsing@gmail.com>
2019-01-31 20:36:07 +0900

    moved bulk of code to browse.php

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
 <?php
-
 // header is included in includes/includedFiles.php
 include("includes/includedFiles.php");
-
 ?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
+<!-- IDEA: show most played songs -->
+<!-- currently, it is merely a replica of browse.php -->
 
-<div class="gridViewContainer">
-
-	<?php
-		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-		while($row = mysqli_fetch_array($albumQuery)) {
-			/* create an html div element each time it loops through the queryset */
-			echo "<div class='gridViewItem'>
-					<span role='link' tabindex='0' onclick='openPage(\"album.php?albumID=" . $row['albumID'] . "\")'>
-						<img src='" . $row['artworkPath'] . "'>
-
-						<div class='gridViewInfo'>"
-							. $row['albumTitle'] .
-						"</div>
-					</span>
-
-				</div>";
-		}
-	?>
-
-</div>
+<script> openPage("browse.php") </script>
