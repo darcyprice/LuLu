eb291ee3e03c38355223fc58ecb57a76887bf194
darcyprice <darcyelsing@gmail.com>
2019-01-31 20:52:17 +0900

    created file

---
+++ artist.php
@@ -0,0 +1,28 @@
+<?php
+include("includes/includedFiles.php");
+
+if(isset($_GET['artistID'])) {
+	$artistID = $_GET['artistID'];
+}
+else {
+  // IDEA: include 'Artist couldn't be found' page
+	header("Location: index.php");
+}
+
+$artist = new Artist($con, $artistID);
+?>
+
+<div class="entityInfo">
+  <div class="centerSection">
+    <div class="artistInfo">
+      <h1 class="artistName">
+        <?php
+          echo $artist->getName();
+        ?>
+      </h1>
+      <div class="headerButtons">
+        <button class="button">Play</button>
+      </div>
+    </div>
+  </div>
+</div>
