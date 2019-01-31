3e0deb6c668044ddcfa0b3bb09ad8ed0706d9dd2
darcyprice <darcyelsing@gmail.com>
2019-01-31 19:29:19 +0900

    added include()

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -1,18 +1,23 @@
+<?php
+
 // in terms of URL entry, the script checks whether the request has been sent by ajax, or
 // whether the User manually entered the URL
 
-<?php
-
 // check whether the url request was from ajax
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-  // ENTER CODE
-  // temp:
-  echo "came from ajax";
+  include("includes/config.php");
+  include("includes/classes/Artist.php");
+  include("includes/classes/Album.php");
+  include("includes/classes/Song.php");
 }
 else {
-  // load the header and footer files
   include("includes/header.php");
   include("includes/footer.php");
+  // call openPage()
+  $url = $_SERVER['REQUEST_URI'];
+  echo "<script> openPage('$url') </script>";
+  // prevents from loading the rest of the page
+  exit();
 }
 
 ?>
