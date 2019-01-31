25abea2f31d1cd539f951288297c00020a6a0c7f
darcyprice <darcyelsing@gmail.com>
2019-01-31 20:35:32 +0900

    created file

---
+++ browse.php
@@ -0,0 +1,28 @@
+<?php
+// header is included in includes/includedFiles.php
+include("includes/includedFiles.php");
+?>
+
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+<div class="gridViewContainer">
+
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+		while($row = mysqli_fetch_array($albumQuery)) {
+			// create an html div element each time it loops through the queryset
+			echo "<div class='gridViewItem'>
+					<span role='link' tabindex='0' onclick='openPage(\"album.php?albumID=" . $row['albumID'] . "\")'>
+						<img src='" . $row['artworkPath'] . "'>
+
+						<div class='gridViewInfo'>"
+							. $row['albumTitle'] .
+						"</div>
+					</span>
+
+				</div>";
+		}
+	?>
+
+</div>
